bd87cfdb6be668a0019b230b28bc5983291463ac
M. Rubait <rubait2006@gmail.com>
2025-04-12 00:30:33 +0500

    final_

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/MRubait_zk/', // Replace with your repo name
+  base: '/MRubait_zk/', 
   plugins: [react()],
 })
